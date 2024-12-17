ebdeb8d3e31b77691ebb334a464372c99612ddfa
coco <1570606465@qq.com>
2024-12-17 22:23:29 +0800

    delete

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -1,11 +1,7 @@
 <template>
   <Layout :class="getClass" v-bind="lockEvents">
-    <LayoutFeatures />
-    <LayoutHeader fixed v-if="getShowFullHeaderRef" />
     <Layout :class="[layoutClass]">
-      <LayoutSideBar v-if="getShowSidebar || getIsMobile" />
       <Layout :class="`${prefixCls}-main`">
-        <LayoutMultipleHeader />
         <LayoutContent />
       </Layout>
     </Layout>
@@ -15,12 +11,8 @@
 <script lang="ts">
   import { defineComponent, computed, unref, onMounted } from 'vue';
   import { Layout } from 'ant-design-vue';
-  import { createAsyncComponent } from '@/utils/factory/createAsyncComponent';
 
-  import LayoutHeader from './header/index.vue';
   import LayoutContent from './content/index.vue';
-  import LayoutSideBar from './sider/index.vue';
-  import LayoutMultipleHeader from './header/MultipleHeader.vue';
 
   import { useHeaderSetting } from '@/hooks/setting/useHeaderSetting';
   import { useMenuSetting } from '@/hooks/setting/useMenuSetting';
@@ -28,24 +20,13 @@
   import { useLockPage } from '@/hooks/web/useLockPage';
   import { useRootSetting } from '@/hooks/setting/useRootSetting';
   import { useAppInject } from '@/hooks/web/useAppInject';
-  import { useWebSocket } from '@/hooks/web/useWebSocket';
   import { useAppStore } from '@/store/modules/app';
   import { ThemeEnum } from '@/enums/appEnum';
-  import { useRouter } from 'vue-router';
-  import { useUserStore } from '@/store/modules/user';
-  import { useMessage } from '@/hooks/web/useMessage';
-  import { formatToDateTime } from '@/utils/dateUtil';
-  import { h } from 'vue';
-  import { useI18n } from '@/hooks/web/useI18n';
 
   export default defineComponent({
     name: 'DefaultLayout',
     components: {
-      LayoutFeatures: createAsyncComponent(() => import('@/layouts/default/feature/index.vue')),
-      LayoutHeader,
       LayoutContent,
-      LayoutSideBar,
-      LayoutMultipleHeader,
       Layout,
     },
     setup() {
@@ -68,37 +49,6 @@
       });
       const getClass = computed(() => [prefixCls, { [`${unref(getSysBg)}-bg`]: unref(getSysBg) && getDarkMode.value === ThemeEnum.LIGHT }]);
 
-      const { initWebSocket } = useWebSocket();
-      const router = useRouter();
-      const userStore = useUserStore();
-      const { notification } = useMessage();
-      const { t } = useI18n();
-
-      function initPrevLogin() {
-        router.beforeEach(async (_to, from, next) => {
-          const userInfo = userStore.getUserInfo;
-          if (userInfo.prevLogin == 1 && from.path == '/login') {
-            notification?.destroy();
-            notification.open({
-              message: t('sys.login.lastLoginInfo'),
-              description: () =>
-                h('div', { class: 'pt-10px' }, [
-                  h('p', null, `时间： ${formatToDateTime(userInfo.prevLoginTime)}`),
-                  h('p', null, `地点： ${userInfo.prevLoginIPAddressName || ''}`),
-                  h('p', null, `IP： ${userInfo.prevLoginIPAddress || ''}`),
-                ]),
-              placement: 'bottomRight',
-              style: { width: '300px' },
-            });
-          }
-          next();
-        });
-      }
-
-      onMounted(() => {
-        initWebSocket();
-        initPrevLogin();
-      });
 
       return {
         getClass,
